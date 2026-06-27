038f3f065d87e46c37fa2fbbbfe32cf55d01e4ae
ADamian17 <adonismartin90@gmail.com>
2026-06-27 17:29:18 -0400

    added path alias

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import './assets/styles/main.css'
+import '@/assets/styles/main.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div className="text-center text-2xl font-bold">Hello world</div>
+    <div className="text-center text-2xl font-bold text-red-500">Hello world</div>
   </StrictMode>,
 )
