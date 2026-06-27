ff4dd057751dbc31536cbde4b35a62e9df4c623d
ADamian17 <adonismartin90@gmail.com>
2026-06-27 17:24:26 -0400

    added tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import './assets/styles/main.css'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <>Hello world</>
+    <div className="text-center text-2xl font-bold">Hello world</div>
   </StrictMode>,
 )
